0d89b8afc19476f7e4dfb77fd6e6a2690b84ff96
Elgars Cerbulis <elgarsc2006@gmail.com>
2025-03-03 09:36:27 +0200

    Add project files.

---
+++ MLproject/ImageData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.ML.Data;
+
+namespace MLproject
+{
+    public class ImageData
+    {
+        [LoadColumn(0)]
+        public string? ImagePath;
+
+        [LoadColumn(1)]
+        public string? Label;
+    }
+}
